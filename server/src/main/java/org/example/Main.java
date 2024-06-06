6f818fd55f74bc328a417aae84cb34e2f26449d2
zengzhimin <15014690709@163.com>
2024-06-06 11:36:53 +0800

    [KF20240606001]-提交代碼

---
+++ server/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * @Description
+ * @Author ZengZhiMin
+ * @Date ${YEAR}-${MONTH}-${DAY} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world1");
+        System.out.println("Hello world1");
+        System.out.println("Hello world1");
+    }
+}
